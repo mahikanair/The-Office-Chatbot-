8e804e6eda3317e0dc5e0968c863132ef2be3598
Mahika Nair <145975620+mahikanair@users.noreply.github.com>
2024-02-25 18:51:01 +0530

    Update streamlit_office_chatbot.py

--- streamlit_office_chatbot.py
+++ streamlit_office_chatbot.py
@@ -7,12 +7,10 @@ import warnings
 import yaml
 warnings.filterwarnings("ignore")
 
-#get openai api key 
-with open('./myauth.yaml', "r") as f:
-    credentials = yaml.safe_load(f)
-openai_api_key = credentials.get('openai', {}).get('access_key')
-gemini_api_key = credentials.get('gemini', {}).get('access_key')
-yt_api_key = credentials.get('youtube', {}).get('access_key')
+#for privacy reasons getting api keys this way 
+openai_api_key = st.secrets['OPENAI']
+gemini_api_key = st.secrets['GEMINI']
+yt_api_key = st.secrets['YOUTUBE']
 
 embeddings = OpenAIEmbeddings(openai_api_key = openai_api_key)
 db = Chroma(persist_directory='./db', embedding_function=embeddings) 
